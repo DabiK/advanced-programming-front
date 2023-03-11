f0fbb4f37ccbfcab1621f94a838d8027712a6d37
unknown <nathan.manaranche@gmail.com>
2023-03-11 13:55:02 +0100

    Creating form to add internship

---
+++ src/components/internship/add-internship-form.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator';
+
+export default class AddInternshipForm extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
