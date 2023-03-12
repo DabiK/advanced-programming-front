fc1cb923bf6c900cf1e704bc19206a0319263703
DabiK <DabiK@users.noreply.github.com>
2023-03-12 21:34:55 +0000

    deploy: 9435d06d085c53ca251e851b44da2069ac4bdcd9

---
+++ _nuxt/static/1678656891/fr/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fadvanced-programming-front\u002F_nuxt\u002Fstatic\u002F1678656891",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Ffr",config:{_app:{basePath:"\u002Fadvanced-programming-front\u002F",assetsPath:"\u002Fadvanced-programming-front\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1678656891/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fsignup","\u002Flogin","\u002Ffr\u002Flogin","\u002Ffr\u002Fsignup"]})
